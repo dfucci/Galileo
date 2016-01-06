23645804ee1ba04276668ed3f22decb67462414a
dfucci <davfuc@gmail.com>
2016-01-06 13:19:12 +0100

    move toolbar from component to layout

--- app/assets/javascripts/components/experiment_details.jsx
+++ app/assets/javascripts/components/experiment_details.jsx
@@ -2,7 +2,6 @@ this.ExperimentDetail = React.createClass({
   render(){
     return (
       <div>
-        <Toolbar/>
         <div className="container">
           <a className="pull-right" href=""><span className="glyphicon glyphicon-plus"></span> Add collaborators</a>
           <h1>{this.props.experiment.title} <span className="label label-default">{this.props.experiment.status}</span> </h1>
