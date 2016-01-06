402c7e0dde9b05b64558c605db8a8b6e9fe4e522
dfucci <davfuc@gmail.com>
2016-01-06 12:16:25 +0100

    Experiment Details component

--- app/assets/javascripts/components/experiment_details.jsx
+++ app/assets/javascripts/components/experiment_details.jsx
@@ -2,8 +2,11 @@ this.ExperimentDetail = React.createClass({
   render(){
     return (
       <div>
-        <a className="pull-right" href=""><span className="glyphicon glyphicon-plus"></span> Add collaborators</a>
-        <h1>{this.props.experiment.title} <span className="label label-default">{this.props.experiment.status}</span> </h1>
+        <Toolbar/>
+        <div className="container">
+          <a className="pull-right" href=""><span className="glyphicon glyphicon-plus"></span> Add collaborators</a>
+          <h1>{this.props.experiment.title} <span className="label label-default">{this.props.experiment.status}</span> </h1>
+        </div>
       </div>
     )
 
