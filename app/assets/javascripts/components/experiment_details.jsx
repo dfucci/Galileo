48cff4bb2184a1566aea53d9cd7bffa65b512912
dfucci <davfuc@gmail.com>
2016-02-19 17:42:05 +0200

    first implementation of several expeirmental phases

--- app/assets/javascripts/components/experiment_details.jsx
+++ app/assets/javascripts/components/experiment_details.jsx
@@ -11,7 +11,6 @@ this.ExperimentDetail = React.createClass({
 
   render(){
     return (
-
         <div className="container">
           <a className="pull-right" href=""><span className="glyphicon glyphicon-plus"></span> Add collaborators</a>
           <h1 className="page-header">{this.props.experiment.title}</h1>
@@ -20,12 +19,60 @@ this.ExperimentDetail = React.createClass({
             <Phases status={this.props.experiment.status}/>
           </div>
           <div className="col-md-10">
+            {(()=>{
+              switch (this.props.experiment.status) {
+                case "planning" : return <ExperimentPlanning />
+                case "scoping" : return <ExperimentScoping />
+                case "executing" : return <ExperimentExecuting />
+                case "analyzing" : return  <ExperimentAnalyzing />
+                case "reporting" : return <ExperimentReporting />
+              }
+            })()}
             <Notes handleEditNotes={this.updateNotes} notes={this.props.experiment.notes} />
           </div>
           </div>
         </div>
-
     )
+  }
+});
 
+
+this.ExperimentReporting = React.createClass({
+  render(){
+    return(
+      <div>Experiment reporting according to Wohlin</div>
+    )
+  }
+});
+
+this.ExperimentAnalyzing = React.createClass({
+  render(){
+    return(
+      <div>Experiment analyzing according to Wohlin</div>
+    )
+  }
+});
+
+this.ExperimentExecuting = React.createClass({
+  render(){
+    return(
+      <div>Experiment executing according to Wohlin</div>
+    )
+  }
+});
+
+this.ExperimentScoping = React.createClass({
+  render(){
+    return(
+      <div>Experiment scoping according to Wohlin</div>
+    )
+  }
+});
+
+this.ExperimentPlanning = React.createClass({
+  render(){
+    return(
+      <div>Experiment planning according to Wohlin</div>
+    )
   }
 })
