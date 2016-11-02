9a81d77f302b6fd88d927aacf842d4a560eaba5a
dfucci <davfuc@gmail.com>
2016-11-02 17:10:18 +0200

    add plus sign to header

--- app/assets/javascripts/components/experiments.jsx
+++ app/assets/javascripts/components/experiments.jsx
@@ -1,26 +1,29 @@
 this.Experiments = React.createClass({
-  getInitialState(){
-    return {experiments: this.props.data};
-  },
+	getInitialState(){
+		return {experiments: this.props.data};
+	},
 
-  getDefaultProps(){
-    return {records: []};
-  },
+	getDefaultProps(){
+		return {records: []};
+	},
 
-  renderExperiments(){
-    return this.state.experiments.map((experiment)=>{
-      return <Experiment key={experiment.id} experiment={experiment} />
-    });
-  },
+	renderExperiments(){
+		return this.state.experiments.map((experiment)=>{
+			return <Experiment key={experiment.id} experiment={experiment} />
+		});
+	},
 
-  render(){
-    return(
-      <div className='experiments'>
-        <h2 className='page-header'>
-          Experiments
-        </h2>
-        {this.renderExperiments()}
-      </div>
-    )
-  }
+	render(){
+		return(
+			<div className='experiments'>
+				<div className='page-header'>
+					<div className="pull-right">
+						<i className="fa fa-plus" aria-hidden="true"></i>
+					</div>
+					<h2> Experiments </h2>
+				</div>
+				{this.renderExperiments()}
+			</div>
+		)
+	}
 })
